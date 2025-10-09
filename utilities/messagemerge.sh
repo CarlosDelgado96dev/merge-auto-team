44d397a3a054ce2a2450afc2d57e13e2f814778e
cdelgadb <cdelgadb@emeal.nttdata.com>
2025-10-09 13:26:28 +0200

    TEST: proove correct merge in master

--- utilities/messagemerge.sh
+++ utilities/messagemerge.sh
@@ -8,16 +8,16 @@ GIT_DIR=$(git rev-parse --git-dir 2>/dev/null) || { echo "No es un repo git"; ex
 
 CHANGELOG="./CHANGELOG.md"
 
-git checkout hot-fix
+git checkout master
 
-git add -A
+git merge --no-ff --no-commit hot-fix
 
 # Usamos HEAD como MERGE_HEAD para no introducir un SHA inválido
-HEAD_SHA=$(git rev-parse HEAD)
+    #HEAD_SHA=$(git rev-parse HEAD)
 
 # Escribimos MERGE_HEAD y MERGE_MSG para simular un merge en curso
-printf "%s\n" "$HEAD_SHA" > "$GIT_DIR/MERGE_HEAD"
-printf "%s\n" "$MSG" > "$GIT_DIR/MERGE_MSG"
+    #printf "%s\n" "$HEAD_SHA" > "$GIT_DIR/MERGE_HEAD"
+    #printf "%s\n" "$MSG" > "$GIT_DIR/MERGE_MSG"
 
-echo "Simulado 'merge in progress'. Mensaje escrito en $GIT_DIR/MERGE_MSG"
-echo "Ahora abre/actualiza Visual Studio Source Control para ver el pre-commit."
+#echo "Simulado 'merge in progress'. Mensaje escrito en $GIT_DIR/MERGE_MSG"
+#echo "Ahora abre/actualiza Visual Studio Source Control para ver el pre-commit."
